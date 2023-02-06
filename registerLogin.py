04c0cda165969356a3df8bef46471ed40b516fb3
DylanRayHastings <122847813+DylanRayHastings@users.noreply.github.com>
2023-02-05 22:13:20 -0600

    Update registerLogin.py

--- registerLogin.py
+++ registerLogin.py
@@ -10,43 +10,72 @@
 
 # BUG : Title of the application should change from Login | Pong on the Login Screen and change to Register on the Register Screen.
 # BUG : Buttons from Login Screen and Register Screen are slightly off-positioned.
-# BUG : Get the Registration to work
-# BUG : Refactor the code to make it more readable and understandable by adding comments and meaningful variable names. START WITH MainApplication() 
-import tkinter as tk # GUI module
+# BUG :  Registration is not working at all.
+# give me a long description for a READ-ME file
+
+
+import tkinter as tk #  MODULES FOR GUI
 import tkinter.ttk as ttk
-import sqlite3 # Database connectivity
-import bcrypt # Hashing password
+from tkinter import messagebox # MESSAGE BOXES 
+from tkinter import PhotoImage # IMAGES
+import sqlite3 # DATABASE CONNECTIVITY
+import webbrowser # WEB CONNECTIVITY 
+import subprocess # SUBPROCESS
+from PIL import Image # IMAGE PROCESSING
+import time # TIME
+import bcrypt
 import logging
 
-# Configure logging for the app
+# Configuring logging for the app
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 class MainApplication(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        # Initialize the Tk class
-        tk.Tk.__init__(self, *args, **kwargs)
+    """
+    The MainApplication class is a Tkinter GUI based application that acts as a container for the login and register screens.
 
-        # Container frame to hold all other frames
+    Attributes:
+    container: A frame widget that acts as the parent container for other frames in the application.
+    frames: A dictionary that maps the frame classes to their instances.
+
+    Methods:
+    __init__: Initializes the Tkinter window and creates the container frame widget. It also creates instances of the LoginScreen and RegisterScreen frames and adds them to the frames dictionary.
+    show_frame: Raises the specified frame to the top of the window stack and makes it visible to the user.
+    """
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
 
-        # Dictionary to store all the frames
         self.frames = {}
         for F in (LoginScreen, RegisterScreen):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky='nsew')
 
-        # Show the login screen first
         self.show_frame(LoginScreen)
 
-    # Raise the specified frame to the front
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 class LoginScreen(tk.Frame):
+    """
+    LoginScreen class is a tkinter GUI based frame that provides functionality for user login.
+
+    Attributes:
+    parent: parent widget for the frame
+    controller: object of MainApplication class
+
+    Methods:
+    __init__: initializes the frame and creates the necessary widgets for the login screen
+    login: connects to the database, checks if the provided username and password match any existing user, and outputs the result
+    clear_username: clears the username entry widget if it contains the placeholder text
+    set_username: sets the placeholder text in the username entry widget if it's empty
+    clear_password: clears the password entry widget if it contains the placeholder text
+    set_password: sets the placeholder text in the password entry widget if it's empty
+    """
     def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.font = ('Courier', 20)
@@ -61,19 +90,21 @@ class LoginScreen(tk.Frame):
         self.password_entry.bind('<FocusIn>', self.clear_password)
         self.password_entry.grid(row=1, column=0, padx=10, pady=10)
 
-        ttk.Button(self, text='Login', command=self.login).grid(row=2, column=0, padx=5, pady=10, sticky='w')
-        ttk.Button(self, text='Register', command=lambda: controller.show_frame(RegisterScreen)).grid(row=2, column=0, padx=5, pady=10, sticky='e')
+        login_button = ttk.Button(self, text='Login', command=self.login)
+        login_button.grid(row=2, column=0, padx=5, pady=10, sticky='w')
+        register_button = ttk.Button(self, text='Register', command=lambda: self.controller.show_frame(RegisterScreen))
+        register_button.grid(row=2, column=0, padx=5, pady=10, sticky='e')
 
     def login(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
+        self.username = self.username_entry.get()
+        self.password = self.password_entry.get()
 
-        # Hash password
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        # Use bcrypt to hash the password before storing in the database
+        hashed_password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
 
         conn = sqlite3.connect('user_information.db')
         c = conn.cursor()
-        c.execute('SELECT * FROM users WHERE username=? AND password=?', (username, hashed_password))
+        c.execute('SELECT * FROM users WHERE username=? AND password=?', (self.username, hashed_password))
         user = c.fetchone()
         conn.close()
 
@@ -101,6 +132,115 @@ class LoginScreen(tk.Frame):
 
     def set_password(self, event):
         if self.password_entry.get() == '':
-            self.password_entry
+            self.password_entry.insert(0, 'Password')
+        self.password_entry.unbind('<FocusOut>')
+    
+    def on_raise(self):
+        self.username_entry.delete(0, tk.END)
+        self.username_entry.insert(0, 'Username')
+        self.password_entry.delete(0, tk.END)
+        self.password_entry.insert(0, 'Password')
+    
+
+class RegisterScreen(tk.Frame):
+    """
+    RegisterScreen class is a tkinter GUI based frame that provides functionality for user registration.
+
+    Attributes:
+    parent: parent widget for the frame
+    controller: object of MainApplication class
+
+    Methods:
+    __init__: initializes the frame and creates the necessary widgets for the register screen
+    register: connects to the database, inserts the new user's information into the database, and outputs the result
+    clear_username: clears the username entry widget if it contains the placeholder text
+    set_username: sets the placeholder text in the username entry widget if it's empty
+    clear_password: clears the password entry widget if it contains the placeholder text
+    set_password: sets the placeholder text in the password entry widget if it's empty
+    clear_confirm_password: clears the confirm password entry widget if it contains the placeholder text
+    set_confirm_password: sets the placeholder text in the confirm password entry widget if it's empty
+    """
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.font = ('Courier', 20)
+
+        self.username_entry = ttk.Entry(self, font=self.font)
+        self.username_entry.insert(0, 'Username')
+        self.username_entry.bind('<FocusIn>', self.clear_username)
+        self.username_entry.grid(row=0, column=0, padx=10, pady=10)
+
+        self.password_entry = ttk.Entry(self, font=self.font, show='*')
+        self.password_entry.insert(0, 'Password')
+        self.password_entry.bind('<FocusIn>', self.clear_password)
+        self.password_entry.grid(row=1, column=0, padx=10, pady=10)
+
+        register_button = ttk.Button(self, text='Register', command=self.register)
+        register_button.grid(row=2, column=0, padx=10, pady=10, sticky='w')
+        back_button = ttk.Button(self, text='Back', command=lambda: self.controller.show_frame(LoginScreen))
+        back_button.grid(row=2, column=0, padx=10, pady=10, sticky='e')
+
+        self.winfo_toplevel().title("Register")
+
+    def clear_username(self, event):
+        if self.username_entry.get() == 'Username':
+            self.username_entry.delete(0, tk.END)
+        self.username_entry.unbind('<FocusOut>')
+        self.username_entry.bind('<FocusOut>', self.set_username)
+
+    def set_username(self, event):
+        if self.username_entry.get() == '':
+            self.username_entry.insert(0, 'Username')
+        self.username_entry.unbind('<FocusOut>')
+
+    def clear_password(self, event):
+        if self.password_entry.get() == 'Password':
+            self.password_entry.delete(0, tk.END)
+        self.password_entry.unbind('<FocusOut>')
+        self.password_entry.bind('<FocusOut>', self.set_password)
+
+    def set_password(self, event):
+        if self.password_entry.get() == '':
+            self.password_entry.insert(0, 'Password')
+        self.password_entry.unbind('<FocusOut>')
+
+    def register(self):
+        self.username = self.username_entry.get()
+        self.password = self.password_entry.get()
+
+        # Use bcrypt to hash the password before storing in the database
+        hashed_password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
+
+        conn = sqlite3.connect('user_information.db')
+        c = conn.cursor()
+        c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (self.username, hashed_password))
+        conn.commit()
+        conn.close()
+    
+    def on_raise(self):
+        self.username_entry.delete(0, tk.END)
+        self.username_entry.insert(0, 'Username')
+        self.password_entry.delete(0, tk.END)
+        self.password_entry.insert(0, 'Password')
 
 
+class CloseButton(tk.Button):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Button.__init__(self, parent, *args, **kwargs)
+        self.config(text='Close', command=self.quit)
+
+class HomeScreen(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        CloseButton(self).pack(side='right', padx=10)
+        self.label = tk.Label(self, text='Welcome to the home screen')
+        self.label.pack(pady=10)
+
+
+app = MainApplication()
+# app.overrideredirect(True)
+app.show_frame(LoginScreen)
+
+app.title('Login | Pong')
+
+app.mainloop()
